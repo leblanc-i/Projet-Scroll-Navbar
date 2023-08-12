4f0b059466998c6d0e2a643c008986fe0c937cd9
idriss <idrissaamadouousmane@gmail.com>
2023-08-12 13:31:50 +0100

    la nav s'apparait quand on scroll vers le haut et disparait quand on scroll vers le bas avec plus de details

--- index.js
+++ index.js
@@ -6,15 +6,17 @@
 
 // Connaitre niveau de scroll (window.scrollY)
 
-let lastScroll = 0; // cette variable stock la valeur de la derniere position du scroll
+let lastScroll = 0; // on cree une variable initialiser à zero(0) qui va stocker la derniere position de notre scroll.
 
 window.addEventListener("scroll", () => {
+    // Quand la position actuelle du scroll est inferieur à la position de la derniere fois qu'on a scroller execute le code suivant.
     if (window.scrollY < lastScroll) {
         navbar.style.top = 0;
+    // Sinon la nav reste à sa position initiale.
     } else {
         navbar.style.top = "-60px";
     }
 
-    lastScroll = window.scrollY;
+    lastScroll = window.scrollY; // on affecte à lastScroll la valeur de la derniere position du scroll elle est differente de la position actuelle.
 });
 
